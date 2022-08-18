c7b77d8b68e846299253301b2fdd9bd0f7ca95e0
EdC <22207434+E-dC@users.noreply.github.com>
2022-08-18 21:48:54 +0200

    style: black formatting

--- progeny_core/cli/cli_main.py
+++ progeny_core/cli/cli_main.py
@@ -3,7 +3,9 @@ import sys
 import importlib
 
 USAGE = {}
-USAGE['main'] = """Access Progeny CLI tools.
+USAGE[
+    "main"
+] = """Access Progeny CLI tools.
 
 Usage:
   progeny [-h] [serve]
@@ -15,7 +17,9 @@ Optional arguments:
   -h --help             Show this
 """
 
-USAGE['serve'] = """Start a middleware server for Prodigy instances.
+USAGE[
+    "serve"
+] = """Start a middleware server for Prodigy instances.
 
 Usage:
   progeny serve [-h] [--port <port>]
@@ -28,30 +32,30 @@ Optional arguments:
   -p --port=<port>      Set port number [default: 9000].
   -h --help             Show this
 """
-SUBCOMMANDS = [sub for sub in USAGE if sub != 'main']
+SUBCOMMANDS = [sub for sub in USAGE if sub != "main"]
+
 
 def parse_main():
-    modified_argv = [
-        arg for arg in sys.argv[1:]
-        if arg in SUBCOMMANDS + ['--help', '-h']
-    ]
-    return docopt.docopt(USAGE['main'], argv=modified_argv, help=False)
+    modified_argv = [arg for arg in sys.argv[1:] if arg in SUBCOMMANDS + ["--help", "-h"]]
+    return docopt.docopt(USAGE["main"], argv=modified_argv, help=False)
+
 
 def run(subcommand, args):
-    m = importlib.import_module(f'progeny_core.cli.cli_{subcommand}')
+    m = importlib.import_module(f"progeny_core.cli.cli_{subcommand}")
     args.pop(subcommand)
     m.run(args)
 
+
 def main():
     args = parse_main()
-    subcommand = 'main'
+    subcommand = "main"
     for sub in SUBCOMMANDS:
         if args[sub]:
             args = docopt.docopt(USAGE[sub], help=True)
             subcommand = sub
             break
     else:
-        print(USAGE['main'])
+        print(USAGE["main"])
         sys.exit()
 
     run(subcommand, args)
--- progeny_core/cli/cli_serve.py
+++ progeny_core/cli/cli_serve.py
@@ -6,46 +6,47 @@ import json
 from loguru import logger
 import sys
 
-fmt = "[{time}] [{level: <8}] ({name: ^15}) | [{host}]: {request} {message} {status} {byte}"
+fmt = (
+    "[{time}] [{level: <8}] ({name: ^15}) | [{host}]: {request} {message} {status} {byte}"
+)
 
 logger.add(
-    sys.stdout,
-    format=fmt,
-    filter='cli_serve',
-    level='DEBUG',
-    colorize=True,
-    enqueue=True
+    sys.stdout, format=fmt, filter="cli_serve", level="DEBUG", colorize=True, enqueue=True
 )
-logger.add("logs.log", level='DEBUG', enqueue=True)
+logger.add("logs.log", level="DEBUG", enqueue=True)
 
 
-app = sanic.Sanic('middleware_progeny')
+app = sanic.Sanic("middleware_progeny")
+
 
 def make_response(res, res_type):
-    if res_type == 'json':
+    if res_type == "json":
         return sanic.response.json(res.json())
-    elif res_type == 'html':
+    elif res_type == "html":
         return sanic.response.html(res.text)
-    elif res_type == 'raw':
+    elif res_type == "raw":
         return sanic.response.raw(res.content)
-    elif res_type == 'text':
+    elif res_type == "text":
         return sanic.response.text(res.text)
 
-def raw_res(res):
-    return make_response(res, 'raw')
-def html_res(res):
-    return make_response(res, 'html')
-def json_res(res):
-    return make_response(res, 'json')
-def text_res(res):
-    return make_response(res, 'text')
 
-def fetch(
-        method, req, pathf,
-        protocol='http',
-        host='0.0.0.0',
-        json=False,
-        **kwargs):
+def raw_res(res):
+    return make_response(res, "raw")
+
+
+def html_res(res):
+    return make_response(res, "html")
+
+
+def json_res(res):
+    return make_response(res, "json")
+
+
+def text_res(res):
+    return make_response(res, "text")
+
+
+def fetch(method, req, pathf, protocol="http", host="0.0.0.0", json=False, **kwargs):
 
     port = req.ctx.port
     payload = {}
@@ -53,109 +54,118 @@ def fetch(
         payload = req.json
 
     url = f'{protocol}://{host}:{port}/{pathf.format(**kwargs).lstrip("/")}'
-    if method == 'get':
+    if method == "get":
         return requests.get(url)
-    elif method == 'post':
+    elif method == "post":
         return requests.post(url, json=payload)
 
 
 @app.middleware("request")
 async def set_port_from_cookie(request):
-    log_port = 'no prodigy port found'
+    log_port = "no prodigy port found"
     try:
-        request.ctx.port = request.cookies.get('prodigy_port')
-        log_port = f'{request.ctx.port}'
+        request.ctx.port = request.cookies.get("prodigy_port")
+        log_port = f"{request.ctx.port}"
     except:
         pass
     request.ctx.log_info = f'{request.headers.get("x-forwarded-for", "unknown client ip")} - {log_port} - {request.path}'
     logger.debug(request.ctx.log_info)
 
-@app.get('/start_session/<port>')
+
+@app.get("/start_session/<port>")
 def start_session(request, port):
-    res = sanic.response.redirect('/mylayerserver')
-    res.cookies['prodigy_port'] = port
-    res.cookies['prodigy_port']['expires'] = datetime.datetime(
+    res = sanic.response.redirect("/mylayerserver")
+    res.cookies["prodigy_port"] = port
+    res.cookies["prodigy_port"]["expires"] = datetime.datetime(
         year=9999, month=12, day=31, hour=23, minute=59, second=59
     )
     return res
 
-@app.get('/mylayerserver')
+
+@app.get("/mylayerserver")
 def get_root(request):
-    res = html_res(fetch('get', request, '/'))
-    logger.info(f'{request.ctx.log_info} | Loaded the Prodigy interface') 
+    res = html_res(fetch("get", request, "/"))
+    logger.info(f"{request.ctx.log_info} | Loaded the Prodigy interface")
     return res
 
-@app.get('/fonts/<font_file>')
+
+@app.get("/fonts/<font_file>")
 def get_fonts(request, font_file):
-    return raw_res(
-        fetch('get', request, '/fonts/{font_file}', font_file=font_file)
-    )
+    return raw_res(fetch("get", request, "/fonts/{font_file}", font_file=font_file))
 
-@app.get('/index.html')
+
+@app.get("/index.html")
 def get_index(request):
-    return html_res(fetch('get', request, '/index.html'))
+    return html_res(fetch("get", request, "/index.html"))
 
-@app.get('/bundle.js')
+
+@app.get("/bundle.js")
 def get_bundle(request):
-    return html_res(fetch('get', request, '/bundle.js'))
+    return html_res(fetch("get", request, "/bundle.js"))
 
-@app.get('/favicon.ico')
+
+@app.get("/favicon.ico")
 def get_favicon(request):
-    return raw_res(fetch('get', request, '/favicon.ico'))
+    return raw_res(fetch("get", request, "/favicon.ico"))
 
-@app.get('/version')
+
+@app.get("/version")
 def get_version(request):
-    return json_res(fetch('get', request, '/version'))
+    return json_res(fetch("get", request, "/version"))
 
-@app.get('/get_questions')
+
+@app.get("/get_questions")
 def get_questions(request):
-    res = json_res(fetch('get', request, '/get_questions'))
+    res = json_res(fetch("get", request, "/get_questions"))
     logger.info(
         f"{request.ctx.log_info} | {len(json.loads(res.body)['tasks'])} new tasks fetched"
     )
     return res
 
-@app.get('/project')
+
+@app.get("/project")
 def get_project(request):
-    return json_res(fetch('get', request, '/project'))
+    return json_res(fetch("get", request, "/project"))
 
-@app.get('/project/<session_id>')
+
+@app.get("/project/<session_id>")
 def get_project_session(request, session_id):
-    return json_res(
-        fetch('get', request, '/project/{session_id}', session_id=session_id)
-    )
+    return json_res(fetch("get", request, "/project/{session_id}", session_id=session_id))
 
-@app.post('/get_session_questions')
+
+@app.post("/get_session_questions")
 def get_session_questions(request):
-    res = json_res(fetch('post', request, '/get_session_questions', json=True))
+    res = json_res(fetch("post", request, "/get_session_questions", json=True))
     logger.info(
-        f"{request.ctx.log_info} | {len(json.loads(res.body)['tasks'])} new tasks fetched")
+        f"{request.ctx.log_info} | {len(json.loads(res.body)['tasks'])} new tasks fetched"
+    )
     return res
 
-@app.post('/set_session_aliases')
+
+@app.post("/set_session_aliases")
 def set_session_aliases(request):
-    return json_res(fetch('post', request, '/set_session_aliases', json=True))
+    return json_res(fetch("post", request, "/set_session_aliases", json=True))
 
-@app.post('/end_session')
+
+@app.post("/end_session")
 def end_session(request):
-    return json_res(fetch('post', request, '/end_session', json=True))
+    return json_res(fetch("post", request, "/end_session", json=True))
 
-@app.post('/validate_answer')
+
+@app.post("/validate_answer")
 def validate_answer(request):
-    return json_res(fetch('post', request, '/validate_answer', json=True))
+    return json_res(fetch("post", request, "/validate_answer", json=True))
 
-@app.post('/give_answers')
+
+@app.post("/give_answers")
 def give_answers(request):
-    n = len(json.loads(request.body)['answers'])
-    res = json_res(fetch('post', request, '/give_answers', json=True))
+    n = len(json.loads(request.body)["answers"])
+    res = json_res(fetch("post", request, "/give_answers", json=True))
     logger.info(f"{request.ctx.log_info} | {n} answers to be sent to DB")
     return res
 
-def run(args):
-    port = args.get('--port', 9000)
 
-    app.run(
-        host='0.0.0.0',
-        port=port,
-        debug=True
-    )
+def run(args):
+    port = args.get("--port", 9000)
+
+    app.run(host="0.0.0.0", port=port, debug=True)
